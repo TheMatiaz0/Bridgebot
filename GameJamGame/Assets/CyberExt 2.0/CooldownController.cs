47896a877257eab5412bb4609ed0222226d9cdee
Biegus <michalbiegaj0@wp.pl>
2020-03-31 11:24:37 +0200

    .

---
+++ GameJamGame/Assets/CyberExt 2.0/CooldownController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cyberevolver;
+using Cyberevolver.Unity;
+using UnityEngine;
+using System.Collections;
+namespace Cyberevolver.Unity
+{
+    public class CooldownController
+    {
+        private readonly AsyncStoper stoper;
+
+
+        public CooldownController(MonoBehaviour main, TimeSpan time, bool cooldownOnStart = false)
+        {
+
+            stoper = new AsyncStoper(main, time);
+            if (cooldownOnStart)
+                Reset();
+        }
+        public CooldownController(MonoBehaviour main, float seconds, bool cooldownOnStart = false) : this(main, TimeSpan.FromSeconds(seconds), cooldownOnStart)
+        {
+
+        }
+        public static CooldownController MakeImmediately(MonoBehaviour main)
+        {
+            return new CooldownController(main, TimeSpan.Zero);
+        }
+        /// <summary>
+        /// Cheks if cooldown pass and reset it if it's true.
+        /// </summary>
+        /// <returns></returns>
+        public bool Try()
+        {
+            if (stoper.MaxTime == TimeSpan.Zero)
+                return true;
+            if (stoper.IsStoperRunning == false)
+            {
+                stoper.Start();
+                return true;
+            }
+            return false;
+        }
+        public void Reset()
+        {
+            stoper.Start();
+        }
+        /// <summary>
+        /// Checks if cooldown pass, but don't reset it.
+        /// </summary>
+        /// <returns></returns>
+        public bool Check()
+        {
+            return stoper.IsStoperRunning;
+        }
+
+
+
+    }
+}
---
+++ GameJamGame/Assets/Script/Player.cs
@@ -0,0 +1,42 @@
+﻿using Cyberevolver.Unity;
+using Cyberevolver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviourPlus
+{
+    [SerializeField]
+    private SerializedTimeSpan jumpDelay = TimeSpan.FromSeconds(1);
+    [SerializeField,ResetButton(1)]
+    private float MoveSize = 1;
+    private Cyberevolver.Unity.CooldownController moveCooldown;
+
+    [Auto]
+    public Rigidbody2D Rigidbody2D { get; private set; }
+    protected override void Awake()
+    {
+        base.Awake();
+        moveCooldown = new CooldownController(this, jumpDelay.TimeSpan);
+    }
+    public void TryMove(Direction dir)
+    {
+        if(moveCooldown.Try())
+        {
+            this.Rigidbody2D.MovePosition(this.transform.Get2DPos() + dir.ToVector2());
+        }
+    }
+    protected virtual void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            TryMove(Direction.Up);
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            TryMove(Direction.Down);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            TryMove(Direction.Left);
+        if(Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+            TryMove(Direction.Right);
+    }
+
+}
