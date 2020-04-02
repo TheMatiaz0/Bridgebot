a86b67358b2e4fb4e244aa3a9f7632fc60790f48
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-04-02 15:06:12 +0200

    Bridge in danger

---
+++ GameJamGame/Assets/BridgeWalkIn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeWalkIn : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		collision.gameObject.layer = 8;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		collision.gameObject.layer = 0;
+	}
+}
