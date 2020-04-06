b6c5318e8c62efdff277c4ebc7a9bd03fc3ccba4
w4ty <w4ty@users.noreply.github.com>
2020-04-06 21:21:11 +0200

    Tutorial Part FOUR

--- GameJamGame/Assets/Script/Tutorial.cs
+++ GameJamGame/Assets/Script/Tutorial.cs
@@ -14,8 +14,9 @@ public class Tutorial : MonoBehaviour
 	[SerializeField] private GameObject arrowObj = null;
 	[SerializeField] private GameObject player = null;
 	[SerializeField] private GameObject carrier = null;
+	[SerializeField] private PhaseController phase;
 	// public GameObject tutTrigger;
-	private bool tutAllowed;
+	private int tutStatus;
 
 
 	void UpdateMessage()
@@ -33,8 +34,14 @@ public class Tutorial : MonoBehaviour
 				arrowObj.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + 1);
 				arrowObj.transform.parent = player.transform;
 				break;
+			case 3:
+				Player.Instance.LockMovement = false;
+				break;
 			case 4:
 				arrowObj.SetActive(false);
+				break;
+			case 5:
+				Player.Instance.LockMovement = true;
 				break;
 			case 6:
 				arrowObj.SetActive(true);
@@ -48,11 +55,20 @@ public class Tutorial : MonoBehaviour
 
 			case 9:
 				Player.Instance.LockMovement = false;
-				arrowObj.SetActive(false);
+				arrowObj.transform.position = new Vector2(24f, 0.50f);
+				arrowObj.transform.rotation = Quaternion.Euler(0, 0, -180);
 				tutorialObj.SetActive(false);
+				tutStatus = 1;
 				break;
-
-
+			case 10:
+				arrowObj.SetActive(false);
+				phase.enableUpdate = false;
+				break;
+			case 14:
+				phase.enableUpdate = true;
+				tutorialObj.SetActive(false);
+				Destroy(this);
+				break;
 		}
 	}
 
@@ -63,7 +79,7 @@ public class Tutorial : MonoBehaviour
 
 	private void BridgeSelection_OnBridgeSelected(object sender, Cyberevolver.SimpleArgs<GameObject> e)
 	{
-
+		BattleTutorial();
 	}
 
 	protected void OnDisable()
@@ -71,17 +87,38 @@ public class Tutorial : MonoBehaviour
 		BridgeSelection.OnBridgeSelected -= BridgeSelection_OnBridgeSelected;
 	}
 
+	private void BattleTutorial()
+	{
+		Debug.LogError("BATTLE TUTORIAL");
+		tutorialObj.SetActive(true);
+		tutStatus = 2;
+		num = 10;
+		UpdateMessage();
+	}
+
+	private void SkipTutorial()
+	{
+		Player.Instance.LockMovement = false;
+		tutorialObj.SetActive(false);
+		Destroy(this);
+	}
+
 	private void Start()
 	{
+		tutStatus = 0;
 		UpdateMessage();
 	}
 
 	void Update()
     {
-		if (Input.GetKeyUp(KeyCode.Space))
+		if (Input.GetKeyUp(KeyCode.Space) && tutStatus != 1)
 		{
 			num++;
 			UpdateMessage();
+		}
+		if (Input.GetKeyUp(KeyCode.LeftControl) && tutStatus != 1)
+		{
+			SkipTutorial();
 		}
     }
 }
