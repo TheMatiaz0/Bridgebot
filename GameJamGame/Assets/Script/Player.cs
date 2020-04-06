cf7c0c7a66701a6fcad2a342e120839ed7c7def3
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-04-06 22:49:48 +0200

    Fix

--- GameJamGame/Assets/Script/Player.cs
+++ GameJamGame/Assets/Script/Player.cs
@@ -259,6 +259,7 @@ public class Player : AutoInstanceBehaviour<Player>, IHpable
         }
 
         Equipment.Instance?.GetCurrent()?.OnUse();
+        audioSource.PlayOneShot(shoot);
     }
 
     private void OnPause()
