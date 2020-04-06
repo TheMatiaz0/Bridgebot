7a17cdaa2aeb1472e38f14a546872ee354355323
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-04-06 12:50:34 +0200

    Building

--- GameJamGame/Assets/Script/TurretItem.cs
+++ GameJamGame/Assets/Script/TurretItem.cs
@@ -4,35 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretItem : Item
+public class TurretItem : BuildItem
 {
-    public override void OnUse()
-    {
-        base.OnUse();
-        if (PhaseController.Instance.CurrentPhase == PhaseController.Phase.PREPARATION)
-        {
-            if (PlacementController.Instance.CanBuild)
-            {
-                PlacementController.Instance.OnPlace();
-                Equipment.Instance.RemoveItem(this);
-            }
-        }
-
-        else
-        {
-            InfoManager.Instance.InfoObject.SetActive(true);
-            InfoManager.Instance.InfoText.text = "Building allowed only during Preparation.";
-        }
-
-    }
-    public override void OnStartSelect()
-    {
-        base.OnStartSelect();
-        PlacementController.Instance.Activate(Prefab);
-    }
-    public override void OnEndSelect()
-    {
-        base.OnEndSelect();
-        PlacementController.Instance.Deactive();
-    }
 }
